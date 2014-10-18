1eec6fd8e463d0091db45c4b2d3ce5e8458c39f2
Jaime Gil de Sagredo Luna <jaimegildesagredo@gmail.com>
2014-10-18 17:15:42 +0200

    Add ssh key

--- docean/cli.py
+++ docean/cli.py
@@ -3,7 +3,7 @@
 """Docean - command-line client for the DigitalOcean API v2
 
 Usage:
-    docean [options] ssh_keys add <path>
+    docean [options] ssh_keys add <name> <path>
     docean [options] ssh_keys delete <id>
 
 Options:
@@ -11,15 +11,39 @@ Options:
     --access-token <access_token>
 """
 
+import json
+
 from docopt import docopt
+from booby import Model, fields
+from finch import Session, Collection
+from tornado import httpclient, ioloop
 
 
 def main():
     args = _parse_args()
 
+    print args
+
     if args['ssh_keys']:
+        session = Session(
+            httpclient.AsyncHTTPClient(),
+            auth=DigitalOceanAuth(args['--access-token']))
+
+        ssh_keys = SshKeys(session)
+
         if args['add']:
-            print 'Adding ssh key', args['<path>']
+            name = args['<name>']
+            public_key_path = args['<path>']
+
+            print 'Adding ssh key', name, public_key_path
+
+            with open(public_key_path) as public_key_file:
+                ssh_key = SshKey(
+                    name=name,
+                    public_key=public_key_file.read())
+
+            ssh_keys.add(ssh_key, _on_public_key_added)
+            ioloop.IOLoop.instance().start()
 
         if args['delete']:
             print 'Deleting ssh key', args['<id>']
@@ -27,3 +51,35 @@ def main():
 
 def _parse_args():
     return docopt(__doc__)
+
+
+def _on_public_key_added(ssh_key, error):
+    ioloop.IOLoop.instance().stop()
+
+    if error is not None:
+        raise error
+
+    print ssh_key
+
+
+class SshKey(Model):
+    id = fields.Field(primary=True)
+    name = fields.Field()
+    public_key = fields.Field()
+    fingerprint = fields.Field()
+
+    def decode(self, response):
+        return json.loads(response.body)['ssh_key']
+
+
+class SshKeys(Collection):
+    model = SshKey
+    url = 'https://api.digitalocean.com/v2/account/keys'
+
+
+class DigitalOceanAuth(object):
+    def __init__(self, access_token):
+        self._access_token = access_token
+
+    def __call__(self, request):
+        request.headers['Authorization'] = 'Bearer {}'.format(self._access_token)
